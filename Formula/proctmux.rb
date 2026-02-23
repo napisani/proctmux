0d383c939ef213c5344cb130af80b1938da603e3
Nick Pisani <napisani@yahoo.com>
2026-02-22 20:39:39 -0500

    brew: update formula to v0.1.7

--- Formula/proctmux.rb
+++ Formula/proctmux.rb
@@ -1,26 +1,26 @@
 class Proctmux < Formula
   desc "tmux-based process manager with interactive TUI"
   homepage "https://github.com/napisani/proctmux"
-  version "0.1.6"
+  version "0.1.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/napisani/proctmux/releases/download/v#{version}/proctmux-darwin-arm64.tar.gz"
-      sha256 "d7ede3f39cdbbd78adf31b3b36a1882d81023bbfba1a44fba235c7b0d3c96126"
+      sha256 "0af007c9c26a1521f0fe732a55284fe8c0a141735e918d30eeefddf537edafd0"
     else
       url "https://github.com/napisani/proctmux/releases/download/v#{version}/proctmux-darwin-amd64.tar.gz"
-      sha256 "e0284b8a79c351f78c643aa4029c94b0882152b5d5b60923ea70301bdcd99504"
+      sha256 "ee3c7cee1cfc3da0b682a57c7f7a9d9337308a52f7d0e0d014a92856487b67c5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/napisani/proctmux/releases/download/v#{version}/proctmux-linux-arm64.tar.gz"
-      sha256 "6754c2f95b0bb6a891999aa342fb2a27b4d796d9eddd611b9998c6da2dad4773"
+      sha256 "e1f7d3e65d73c1463f8769fbd3b73245c9d7bc236595b55bcb8565b624ddeb6a"
     else
       url "https://github.com/napisani/proctmux/releases/download/v#{version}/proctmux-linux-amd64.tar.gz"
-      sha256 "978ebadd0a70d00b30d83570123496fa0f29f1699555faf41e6ea99a4b1e97c1"
+      sha256 "d45808e50b668fdf8f6163d3f1d18f5f7aed9e0b713ed3316d6d73b2779d7b64"
     end
   end
 
