026d2758be84ead7d3f49f8ff9791f4f25b647f1
Nick Pisani <napisani@yahoo.com>
2026-06-12 13:40:52 -0400

    test: make version banner test release-aware

--- src/version.zig
+++ src/version.zig
@@ -11,6 +11,6 @@ pub fn banner() []const u8 {
     return app_name ++ " " ++ version;
 }
 
-test "banner includes app name and development version" {
-    try std.testing.expectEqualStrings("proctmux 1.0.0-dev", banner());
+test "banner includes app name and configured version" {
+    try std.testing.expectEqualStrings(app_name ++ " " ++ version, banner());
 }
